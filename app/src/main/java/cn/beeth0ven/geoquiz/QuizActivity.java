4e47dc16b63732304e9b60bbff329138f29d4938
beeth0ven <beeth0vendev@163.com>
2016-12-20 16:52:01 +0800

    Chapter 2 Challenge

--- app/src/main/java/cn/beeth0ven/geoquiz/QuizActivity.java
+++ app/src/main/java/cn/beeth0ven/geoquiz/QuizActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -11,7 +12,8 @@ public class QuizActivity extends AppCompatActivity {
 
     private Button mTrueButton;
     private Button mFalseButton;
-    private Button mNextButton;
+    private ImageButton mNextButton;
+    private ImageButton mPrevButton;
     private TextView mTextView;
 
     private int mQuestionIndex = 0;
@@ -44,16 +46,30 @@ public class QuizActivity extends AppCompatActivity {
             }
         });
 
-        mNextButton = (Button) findViewById(R.id.next_button);
+        mNextButton = (ImageButton) findViewById(R.id.next_button);
         mNextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                increaseQuestionIndex();
-                updateQuestion();
+                nextQuestion();
             }
         });
 
         mTextView = (TextView) findViewById(R.id.question_text_view);
+        mTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                nextQuestion();
+            }
+        });
+
+        mPrevButton = (ImageButton) findViewById(R.id.prev_button);
+        mPrevButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                previousQuestion();
+            }
+        });
+
         updateQuestion();
     }
 
@@ -62,8 +78,25 @@ public class QuizActivity extends AppCompatActivity {
     }
 
 
+    private void decreaseQuestionIndex() {
+        mQuestionIndex = (mQuestionIndex - 1) % mQuestions.length;
+        if (mQuestionIndex < 0) {
+            mQuestionIndex += mQuestions.length;
+        }
+    }
+
     private void updateQuestion() {
         mTextView.setText(mQuestions[mQuestionIndex].getTextResId());
+    }
+
+    private void nextQuestion() {
+        increaseQuestionIndex();
+        updateQuestion();
+    }
+
+    private void previousQuestion() {
+        decreaseQuestionIndex();
+        updateQuestion();
     }
 
     private void checkAnswer(boolean isTrue) {
