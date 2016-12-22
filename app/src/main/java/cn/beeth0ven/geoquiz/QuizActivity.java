56aa28e38f3717da0f6117bd2a9ab3a64d344968
beeth0ven <beeth0vendev@163.com>
2016-12-22 17:29:27 +0800

    Debug

--- app/src/main/java/cn/beeth0ven/geoquiz/QuizActivity.java
+++ app/src/main/java/cn/beeth0ven/geoquiz/QuizActivity.java
@@ -2,12 +2,15 @@ package cn.beeth0ven.geoquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
+
+    private static final String TAG = "QuizActivity";
 
     private Button mTrueButton;
     private Button mFalseButton;
@@ -26,7 +29,11 @@ public class QuizActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate");
         setContentView(R.layout.activity_quiz);
+        if (savedInstanceState != null) {
+            mQuestionIndex = savedInstanceState.getInt("mQuestionIndex", 0);
+        }
 
         mTrueButton = (Button) findViewById(R.id.true_button);
         mTrueButton.setOnClickListener(new View.OnClickListener() {
@@ -53,7 +60,7 @@ public class QuizActivity extends AppCompatActivity {
             }
         });
 
-        mTextView = (TextView) findViewById(R.id.question_text_view);
+//        mTextView = (TextView) findViewById(R.id.question_text_view);
         updateQuestion();
     }
 
@@ -75,4 +82,40 @@ public class QuizActivity extends AppCompatActivity {
         ).show();
     }
 
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Log.d(TAG, "onSaveInstanceState");
+        outState.putInt("mQuestionIndex", mQuestionIndex);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy");
+    }
 }
